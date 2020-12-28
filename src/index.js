66521ce8a6c933e104f7a8d8823769f236edc6f1
serg <nikolas>
2020-12-28 19:18:14 +0200

    #1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './components/component'
+import './css/style.css'
+import './scss/main.scss'
+
+// add(5, 6);
+
+console.log('index');
+console.log('index');
+console.log('index');
+console.log('index');
+
+alert('test')
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+const filename = (ext) => isDev ? `[name].${ext}`: `[name].[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: `${filename('js')}`,
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist'
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8000
+    },
+    module: {
+        rules: [
+            {
+              test: /\.css$/i,
+              use: [
+                MiniCssExtractPlugin.loader,  
+                'css-loader'],
+            },
+            {
+                test: /\.s[ca]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    'sass-loader'
+                ],
+              }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            minfy: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `${filename('css')}`
+        })
+    ]
+} 
